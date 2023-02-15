f2642e31bc597c01efc8f02441acf9326e4434be
marcelogmoura <mgmoura@gmail.com>
2023-02-15 15:14:37 -0300

    excecao para cpf e email ja cadastrados

--- src/main/java/com/mgmoura/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/mgmoura/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,14 @@
 package com.mgmoura.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mgmoura.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
+	
+	// optional, pode o nao encontrar a Pessoa
+	Optional<Pessoa> findByCpf(String cpf); 
+	Optional<Pessoa> findByEmail(String email);
 }
