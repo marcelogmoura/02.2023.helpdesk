4f47d3363848b343bbef63bcc93341efe325a0e9
marcelogmoura <mgmoura@gmail.com>
2023-02-16 20:35:12 -0300

    findall para chamado

--- src/main/java/com/mgmoura/helpdesk/services/ChamadoService.java
+++ src/main/java/com/mgmoura/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.mgmoura.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id); // pode ou nao encontrar um chamado
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! id: "+id));
+	}
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 
 }
